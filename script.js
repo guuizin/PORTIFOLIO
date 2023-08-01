c477a8a19c247e81eda4089f6da97b342fe8f34d
Guilherme Felipe <135027309+guuizin@users.noreply.github.com>
2023-08-01 10:45:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+script
+
+
+var typed = new typed("span",{
+    strings:["Elizabeth Elmiz.","web Developer.","UX Designer.","Photographer."],
+    typeSpeed: 70,
+    backSpeed: 60,
+    loop: true
+});
